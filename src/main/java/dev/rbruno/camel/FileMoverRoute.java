2b88a0c9d6bba5eaba6bac3ec39b66d6a3373d0e
Riccardo Bruno <mr.riccardo_bruno@libero.it>
2023-04-26 22:49:38 +0200

    Initial commit

---
+++ src/main/java/dev/rbruno/camel/FileMoverRoute.java
@@ -0,0 +1,15 @@
+package dev.rbruno.camel;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+//@Component
+public class FileMoverRoute extends RouteBuilder {
+    @Override
+    public void configure() throws Exception {
+from("file:///tmp/camel").
+        log("${headers}").
+        log("${body}").
+        to("file:///tmp/camel/processed");
+    }
+}
